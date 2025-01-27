b4f134174d2627099ed6657d5ec188a12101d651
Samuel Onoja <samuelonoja0@gmail.com>
2025-01-27 10:54:50 -0800

    implement the introduction screen

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const nameInput = document.getElementById("name");
+const playNow = document.querySelector(".enter-game");
+
+
+playNow.addEventListener("click", function () {
+  nameInput.style.visibility = "visible";
+  playNow.style.visibility = "hidden";
+});
+
+//let enteredName = "";
+nameInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const enteredName = nameInput.value.trim();
+
+    if (enteredName) {
+      localStorage.setItem("userName", enteredName);
+    }
+    window.location.href = "welcome.html";
+  }
+});
+
